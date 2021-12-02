90bd2657fcfa21c4cbca4a929cfefda1bb055d62
abhishek <vadhadiyaabhishek@gmail.com>
2021-12-02 23:24:35 +0530

    Prices, Contact and About page completed and First page navbar made responsive according to demo website

--- HTML/public/JS/script.js
+++ HTML/public/JS/script.js
@@ -1,3 +1,19 @@
 function hide_policy(){
     document.getElementById('privacy-policy').style.display = "none";
 }
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20 ){
+            $(".navbar").addClass("sticky");
+        }
+        else{
+            $(".navbar").removeClass("sticky");
+        }
+    });
+
+    $('.menu-toggler').click(function(){
+        $(this).toggleClass("active");
+        $('.custom-navbar-menu').toggleClass("active");
+    });
+});
